9e147049569135596c90dae7371fb3a938f576e3
Ramil <rammtw@gmail.com>
2024-03-10 13:08:31 +0300

    contains duplicate

---
+++ array/contains_duplicate.go
@@ -0,0 +1,12 @@
+package array
+
+func ContainsDuplicate(nums []int) bool {
+	set := make(map[int]struct{})
+	for _, num := range nums {
+		if _, hasNum := set[num]; hasNum {
+			return true
+		}
+		set[num] = struct{}{}
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "leetcode-go/array"
 
 func main() {
-	array.TwoSum([]int{41, 123, 515, 124, 525}, 6)
+	//array.TwoSum([]int{41, 123, 515, 124, 525}, 6)
+	array.ContainsDuplicate([]int{321, 321, 111})
 }
